09995bb3a6ccc2fae8a557b2bd3445a343cda3ad
Taven Shumaker <tavensshumaker@gmail.com>
2022-09-21 22:00:39 -0700

    fixed error in clinet.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,6 +1,8 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
 
 /**
@@ -8,9 +10,23 @@ import gql from 'graphql-tag'
  */
 
 
-const link = new HttpLink({
+const http = new HttpLink({
   uri: 'http://localhost:4000/'
 });
+
+const delay = setContext(
+  request =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success()
+      }, 800)
+    })
+)
+
+const link = ApolloLink.from([
+  delay,
+  http
+])
 
 /**
  * The InMemoryCache is built into apollo client, but it can be easily extended
@@ -18,8 +34,8 @@ const link = new HttpLink({
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link,
-  cache
+  cache,
+  link
 });
 
 export default client;

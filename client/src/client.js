dc7eabf1a4f4bd8691b656a835c2970cd499818f
Taven Shumaker <tavensshumaker@gmail.com>
2022-09-21 22:12:35 -0700

    completed optimistic UI setup

--- client/src/client.js
+++ client/src/client.js
@@ -3,13 +3,10 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import { ApolloLink } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
-import gql from 'graphql-tag'
 
 /**
  * Create a new apollo client and export as default
  */
-
-
 const http = new HttpLink({
   uri: 'http://localhost:4000/'
 });
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -30,6 +30,7 @@ const NEW_PET = gql`
 export default function Pets () {
   const [modal, setModal] = useState(false);
   const {data, loading, error} = useQuery(ALL_PETS);
+
   const  [createPet, newPet] = useMutation(
     NEW_PET, {
     update(cache, { data: {addPet}}) {
@@ -39,6 +40,10 @@ export default function Pets () {
         data: { pets: [addPet, ...pets] }
       })
     }
+    /**
+     * You can place optimisticResponse right here, but it won't become optimistic until the response comes back
+     */
+    // optimisticResponse {}
   });
 
 
@@ -47,13 +52,34 @@ export default function Pets () {
     createPet({
       variables: {
         "newPet": input
+      },
+      /**
+       * The other reason that you would probably put the optimistic response down here is becasue you can use inputs to determine what
+       * the optimistic response would look like
+       * 
+       * This optimistic response is actually being added to the apollo cache, and then when the real response comes back it will
+       * replace what is in the cache. This can become a probleem if you don't clean up on errors. On error you will have to clean up
+       * your optimistic response.
+       */
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: '000000',
+          type: input.type,
+          name: input.name,
+          img: 'https://via.placeholder.com/300',
+        }
       }
     })
   }
 
-  if (loading || newPet.loading) {
-    return <Loader />
-  }
+  /**
+   * We don't need to show the loader because we opted into optimistic UI
+   */
+  // if (loading || newPet.loading) {
+  //   return <Loader />
+  // }
 
   if (error || newPet.error) {
     return <p>Error</p>
